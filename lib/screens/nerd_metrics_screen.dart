b61d5fa3c0fc8a6c22e85dae971bb66717b73515
Ragsie <alexanderbennedsgaardolesen@gmail.com>
2026-09-02 13:36:42 +0200

    scaleing fix

--- lib/screens/nerd_metrics_screen.dart
+++ lib/screens/nerd_metrics_screen.dart
@@ -11,7 +11,7 @@ class NerdMetricsScreen extends StatelessWidget {
       body: ListenableBuilder(
         listenable: rosService,
         builder: (context, _) {
-          // Calculates hours and minutes neatly based on total operating time.
+          // Calculate hours and minutes neatly based on the total operating time
           final int hours = rosService.operatingMinutes ~/ 60;
           final int minutes = rosService.operatingMinutes % 60;
 
